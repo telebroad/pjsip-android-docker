bd53e15490e008942f1eaf4882267574dac78a65
isaac <isaac@telebroad.com>
2024-01-18 13:05:48 -0500

    +1

--- start.ps1
+++ start.ps1
@@ -1,16 +1,9 @@
 
 Set-PSDebug -Trace 1
 
-# stop running containers
-# docker compose stop
 
 
-# the '-f' flag is to not prompt for confirmation
-
-# pull the latest version of the image
-# docker compose pull
-# remove stopped service containers
-# docker compose rm -f
+# build images
 docker compose build 
 # create and start containers
 docker compose up -d --force-recreate 
