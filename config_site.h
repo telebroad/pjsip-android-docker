17ee8d62bf13143c2d3bcc23c178824a25440adb
Yehuda Goldshtein <yehudashtein@gmail.com>
2024-02-28 14:36:10 -0500

    added stun support

--- config_site.h
+++ config_site.h
@@ -1,4 +1,4 @@
-#include <pj/config_site_sample.h>
+
 #define PJ_CONFIG_ANDROID 1
 #define PJMEDIA_HAS_VIDEO 1
 #define PJMEDIA_HAS_AND_MEDIA_AMRNB 1
@@ -7,9 +7,10 @@
 #define PJMEDIA_HAS_AND_MEDIA_VP8 1
 #define PJMEDIA_HAS_AND_MEDIA_VP9 1
 #define PJMEDIA_HAS_ANDROID_MEDIACODEC 1
-
+#define PJMEDIA_STREAM_ENABLE_KA 1
 #define PJMEDIA_HAS_ILBC_CODEC 1
 #define PJMEDIA_HAS_G722_CODEC 1
 // #define PJMEDIA_HAS_GSM_CODEC 1
 #define PJMEDIA_HAS_G729_CODEC 1
 // #define PJMEDIA_HAS_OPUS_CODEC 1
+#include <pj/config_site_sample.h>
