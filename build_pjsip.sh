b3ac8de59fa55ee7d43f5775d08bdd3e1432a1f9
YehudaGoldshtein <yehudago@post.bgu.ac.il>
2024-12-09 19:53:30 +0200

    some changes using ai

--- build_pjsip.sh
+++ build_pjsip.sh
@@ -1,30 +1,27 @@
 #!/bin/sh -e
 
 OPENSSL_OUTPUT_PATH=${WORK_PATH}/openssl_3.4.0_${TARGET_ABI}
-OPUS_OUTPUT_PATH=${WORK_PATH}/opus_v1.5.2_${TARGET_ABI}
+OPUS_OUTPUT_PATH=/pjsip/opus/opus_${TARGET_ABI}  # Adjust if your install path differs
 
+echo "[$(date '+%Y-%m-%d %H:%M:%S %Z')]: PJSIP-CONFIG :: TARGET_ABI=${TARGET_ABI} ./configure-android --use-ndk-cflags --with-ssl=${OPENSSL_OUTPUT_PATH} --with-opus=${OPUS_OUTPUT_PATH} --enable-video" \
+    | tee -a /pjsip/build_pjsip.log
 
-# https://docs.pjsip.org/en/latest/get-started/android/build_instructions.html#building-pjsip
-echo "[$(date '+%Y-%m-%d %H:%M:%S %Z')]: PJSIP-CONFIG :: TARGET_ABI=${TARGET_ABI} ./configure-android --use-ndk-cflags --with-ssl=${OPENSSL_OUTPUT_PATH} --enable-video" \
-    | tee -a /pjsip/build_pjsip.log 
+# Add include and lib paths for Opus so PJSIP can detect it
+CFLAGS="-g -O0 -I${OPUS_OUTPUT_PATH}/include" LDFLAGS="-g -O0 -L${OPUS_OUTPUT_PATH}/lib" \
+    ./configure-android \
+    --use-ndk-cflags \
+    --with-ssl=${OPENSSL_OUTPUT_PATH} \
+    --with-opus=${OPUS_OUTPUT_PATH} \
+    --enable-video \
+    | tee -a /pjsip/build_pjsip.log
 
-
-
-
-CFLAGS="-g -O0" LDFLAGS="-g -O0" ./configure-android --use-ndk-cflags --with-ssl=${OPENSSL_OUTPUT_PATH} --enable-video | tee -a /pjsip/build_pjsip.log 
 echo "[$(date '+%Y-%m-%d %H:%M:%S %Z')]: PJSIP-BUILD :: make dep && make clean && make" \
     | tee -a /pjsip/build_pjsip.log
+
 make dep && make clean && make \
     | tee -a /pjsip/build_pjsip.log
-
 
 cd /pjsip/pjproject/pjsip-apps/src/swig
 echo "Processing element: $TARGET_ABI"
 echo "[$(date '+%Y-%m-%d %H:%M:%S %Z')]: APP-BUILD $TARGET_ABI :: make" >> /pjsip/build_pjsip.log 2>&1
 make | tee -a /pjsip/build_pjsip.log
-
-
-# if [ "$TARGET_ABI" != "arm64-v8a" ]; then
-#     mv /pjsip/pjproject/pjsip-apps/src/swig/java/android/pjsua2/src/main/jniLibs/arm64-v8a \
-#         /pjsip/pjproject/pjsip-apps/src/swig/java/android/pjsua2/src/main/jniLibs/${TARGET_ABI}/
-# fi
